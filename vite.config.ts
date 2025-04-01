7922d9bce3d753576234822f6366c185dd709f39
Brandon Roberts <robertsbt@gmail.com>
2025-04-01 08:57:38 -0500

    chore: update to rolldown-vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,16 @@
 
 import angular from '@analogjs/vite-plugin-angular';
 import { defineConfig } from 'vite';
-import { minify } from "rollup-plugin-esbuild";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: ['es2020'],
-    minify: false
+    target: ['es2020']
   },
   resolve: {
     mainFields: ['module'],
   },
   plugins: [
-    angular(),
-    minify({
-      minify: true,
-      legalComments: "none",
-      target: "es2022",
-    }) as any,
-  ],
-  experimental: {
-    enableNativePlugin: true,
-  },
+    angular()
+  ]
 });
